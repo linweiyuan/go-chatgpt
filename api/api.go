c9d822a0a02669e2b9f318e3ffd4ba19ea1a1e8f
linweiyuan <root@linweiyuan.com>
2023-04-11 05:23:49 +0000

    support continue text in long conversation

--- api/api.go
+++ api/api.go
@@ -116,7 +116,8 @@ func (api *API) StartConversation(content string) {
 			}],
 			"parent_message_id": "%s",
 			"model": "%s",
-			"conversation_id": "%s"
+			"conversation_id": "%s",
+			"continue_text": "continue"
 		}`, uuid.NewString(), common.RoleUser, common.RoleUser, content, parentMessageID, common.ChatGPTModel, common.ConversationID)).Post("/conversation")
 
 	// get it again from response
